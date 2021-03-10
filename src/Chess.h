587058dd326c3da603bca23fe55a50f3c4f5d917
Riley Webb <44430479+TheApplePie@users.noreply.github.com>
2021-03-10 19:39:42 +1100

    test

--- src/Chess.h
+++ src/Chess.h
@@ -11,34 +11,32 @@
 
 enum Pieces
 {
-    Blank = (char)0x0,    //0000 000 0
-    King = (char)0x2,     //0000 001 0
-    Rook = (char)0x4,     //0000 010 0
-    Bishop = (char)0x6,   //0000 011 0
-    Queen = (char)0x8,    //0000 100 0
-    Knight = (char)0xA,   //0000 101 0
-    Pawn = (char)0xC,     //0000 110 0
+    Blank = (char)0x0,  //0000 000 0
+    King = (char)0x2,   //0000 001 0
+    Rook = (char)0x4,   //0000 010 0
+    Bishop = (char)0x6, //0000 011 0
+    Queen = (char)0x8,  //0000 100 0
+    Knight = (char)0xA, //0000 101 0
+    Pawn = (char)0xC,   //0000 110 0
 };
 
 enum Colors
 {
-    White = 0x1,    //0000 000 1
-    Black = 0x0,     //0000 000 0
+    White = 0x1,        //0000 000 1
+    Black = 0x0,        //0000 000 0
 };
 
 enum Directions
 {
-    Up = (-8),
-    Down = (+8),
-    Left = (-1),
-    Right = (+1),
-    UpLeft = (-9),
-    UpRight = (-7),
-    DownLeft = (+7),
-    DownRight = (+9),
+    Up = (char)(-8),
+    Down = (char)(+8),
+    Left = (char)(-1),
+    Right = (char)(+1),
+    UpLeft = (char)(-9),
+    UpRight = (char)(-7),
+    DownLeft = (char)(+7),
+    DownRight = (char)(+9),
 };
-
-
 
 //Vairables
 const char* StartingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
